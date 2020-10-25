fa47eecdd4471213a69cd76ce71dd8da7e0720b3
i_one <opppyshow@gmail.com>
2020-10-25 16:07:21 +0200

    Fixing pop-up window by doing it with jQuery (with functions)

--- 1ON3_profile/js/mscript.js
+++ 1ON3_profile/js/mscript.js
@@ -1,3 +1,4 @@
+"use strict";
 /*Dropdownscript*/
 
 /* When the user clicks on the button, 
@@ -20,25 +21,60 @@
             }
         }
 
-        /* Set the width of the sidebar to 250px (show it) */
-function openNav() {
+        
+        
+/*jQuery code*/
+$(document).ready(function()
+{
+    
+    $('.openbtn').on("click", function(){
+        openPlaylistMenu();
+    });
+    $('.closebtn, .show_popup').on("click", function(){
+        closePlaylistMenu();
+    })
+    $('.btn_create input').on("click", function(){
+        ShowName();
+        openPlaylistMenu();
+        $('.overlay_popup, .popup').hide();
+    });
+
+    $('.show_popup').click(function() { // Вызываем функцию по нажатию на кнопку
+        var popup_id = $('#' + $(this).attr("rel")); // Связываем rel и popup_id
+        $('.popup').show(); // Открываем окно
+        $('.overlay_popup').show(); // Открываем блок заднего фона
+    });
+    $('.overlay_popup').click(function() { // Обрабатываем клик по заднему фону
+        $('.overlay_popup, .popup').hide(); // Скрываем затемнённый задний фон и основное всплывающее окно
+    });
+
+    $("#plylstNm").on('click', function()
+    {
+        
+        closePlaylistMenu();
+    });
+    
+});
+
+
+/**/
+    function ShowName()
+    {
+        /**/
+        let playlistName = $("#playlistName").val();
+        $("#plylstNm").html(playlistName);
+        /**/
+        let playlistDescription = $("playlistDescription").val();
+        $("#plystDscrptn").html(playlistDescription);
+    }
+
+    
+    /* Set the width of the sidebar to 400px (show it) */
+function openPlaylistMenu() {
     document.getElementById("myPlaylists").style.width = "400px";
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
+  function closePlaylistMenu() {
     document.getElementById("myPlaylists").style.width = "0";
   }
-
-/*jQuery code*/
-/*$(document).ready(function()
-{*/
-    function ShowName()
-    {
-        let playlistName = $("#playlistName").val();
-        $("#plylstNm").html(playlistName);
-        // let playlistDescription = $("playlistDescription").val();
-        // $("#plystDscrptn").html(playlistDescription);
-    }
-
-// });
