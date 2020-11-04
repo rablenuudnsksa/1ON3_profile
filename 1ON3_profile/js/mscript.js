4ffe0ab0a2139e58e92eb2da74473407af141a3d
i_one <opppyshow@gmail.com>
2020-11-04 22:54:30 +0200

    Adding playlist and song classes

--- 1ON3_profile/js/mscript.js
+++ 1ON3_profile/js/mscript.js
@@ -1,27 +1,5 @@
 "use strict";
-/*Dropdownscript*/
 
-/* When the user clicks on the button, 
-        toggle between hiding and showing the dropdown content */
-        function myFunction() {
-            document.getElementById("myDropdown").classList.toggle("show");
-        }
-
-        // Close the dropdown if the user clicks outside of it
-        window.onclick = function (event) {
-            if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i];
-                    if (openDropdown.classList.contains('show')) {
-                        openDropdown.classList.remove('show');
-                    }
-                }
-            }
-        }
-
-        
         
 /*jQuery code*/
 $(document).ready(function()
@@ -31,6 +9,8 @@ $(document).ready(function()
         var popup_id = $('#' + $(this).attr("rel")); // Связываем rel и popup_id
         $('.popup_create').show(); // Открываем окно
         $('.overlay_popup_create').show(); // Открываем блок заднего фона
+        closePlaylistMenu();
+
     });
     // Function that HIDES pop-up window
     $('.overlay_popup_create').click(function() { // Обрабатываем клик по заднему фону
@@ -42,10 +22,12 @@ $(document).ready(function()
         var popup_id = $('#' + $(this).attr("rel")); // Связываем rel и popup_id
         $('.popup_edit').show(); // Открываем окно
         $('.overlay_popup_edit').show(); // Открываем блок заднего фона
+        closePlaylistMenu();
     });
     // Function that HIDES pop-up window
     $('.overlay_popup_edit').click(function() { // Обрабатываем клик по заднему фону
         $('.overlay_popup_edit, .popup_edit').hide(); // Скрываем затемнённый задний фон и основное всплывающее окно
+        
     });
 
                             /*BUTTONS*/
@@ -61,9 +43,24 @@ $(document).ready(function()
     // Button "Create" in pop-up window (opens pl menu,
     // hides pop-up window and shows pl name
     $('.btn_create input').on("click", function(){
-        ShowName();
         openPlaylistMenu();
         $('.overlay_popup_create, .popup_create').hide();
+
+
+
+        //Gets playlist name from input
+        let name_playlist = document.getElementById("playlistName").value;
+        //Playlist name
+        playlists[i] = new Playlist(name_playlist); //Creates new playlist
+        console.log(playlists[i].get_playlistName()); //Gets name
+        
+        //Gets playlist description from input
+        let description_playlist = document.getElementById("playlistDescription").value;
+        //Playlist description
+        playlists[i].set_playlistDescription(description_playlist);
+        console.log(playlists[i].get_playlistDescription());
+
+        i++;
     });
 
      // hides pop-up window and shows pl name
@@ -79,58 +76,85 @@ $(document).ready(function()
         closePlaylistMenu();
 
     });
+
+
     
 });
+    //Counter of playlists
+    let i = 0;
+    let playlists = []; //Playlists array
 
 
-    // class Playlist
-    // {
-    //     constructor(playlistName)
-    //     {
-    //         this._playlistName = playlistName;
-    //     }
-        
-    //     get_plName()    //Getter for playlist Name
-    //     {
-    //         return this.playlistName;
-    //     }
-    //     set_playlistName(x)         //Setter for playlist name
-    //     {
-    //         this._playlistName = x;
-    //     }
-        
-    //     //Description
-    //     _playlistDescription;
-    //     set_playlistDescription(x){     //Setter
-    //         this._playlistDescription = x;
-    //     }
-    //     get_playlistDescription()       //Getter
-    //     {
-    //         return this._playlistDescription;
-    //     }
-
-
-    //     arrSongs = [];
-
-    // }
-
-    // class Song extends Playlist
-    // {
-
-    // }
-
-    let playlist1 = new Playlist("Linkin Park");
-/*Function that shows pl name in PL MENU*/
-    function ShowName()
+    /*Playlist CLASS*/
+    class Playlist
     {
-        /*Reads pl NAME*/
-        //let playlistName = $("#playlistName").val();
-        //d$("#plylstNm").html(playlistName);
+        constructor(playlistName)       //Constructor that receive playlist name
+        {
+            this._playlistName = playlistName;
+        }
         
-        // let playlistDescription = $("playlistDescription").val();
-        // $("#plystDscrptn").html(playlistDescription);
+        //Playlist Name
+        get_playlistName()    //Getter for playlist Name
+        {
+            return this._playlistName;
+        }
+        set_playlistName(x)         //Setter for playlist name
+        {
+            this._playlistName = x;
+        }
+        
+        //Description
+        _playlistDescription;
+        set_playlistDescription(x){     //Setter
+            this._playlistDescription = x;
+        }
+        get_playlistDescription()       //Getter
+        {
+            return this._playlistDescription;
+        }
+
+        //Song array
+        arrSongs = [];
+        set_arraySongs (track)  //Setter
+        {
+            this.arrSongs.push(track);
+        }
+
     }
-    
+
+    class Song extends Playlist
+    {
+        constructor(nameSong, artist)   //Constructor receives song name and artist
+        {
+            this.nameSong = nameSong;
+            this.artist = artist;
+        }
+        
+        //Song name
+        get_nameSg() {  //Getter
+            return this.nameSong;
+        }
+        set_nameSg(x){  //Setter
+            this.nameSong = x;
+        }
+
+        //Artist
+        get_artist() {  //Getter
+            return this.artist;
+        }
+        set_artist(x){  //Setter
+            this.artist = x;
+        }
+
+        //URL
+        get_url() { //Getter
+            return this.url;
+        }
+        set_url(x){ //Setter
+            this.url = x;
+        }
+
+    }
 
     
     /* Set the width of the sidebar to 400px (show it) */
@@ -142,3 +166,27 @@ function openPlaylistMenu() {
   function closePlaylistMenu() {
     document.getElementById("myPlaylists").style.width = "0";
   }
+
+  /*Dropdownscript*/
+
+/* When the user clicks on the button, 
+        toggle between hiding and showing the dropdown content */
+    function dropdownScript() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
+        
